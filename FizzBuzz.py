da26d457d913dbde30f42bf208892497ef938d62
Yavuz Murat <115803232+KesshinQ@users.noreply.github.com>
2022-10-14 21:17:05 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,14 @@
 for x in range(0 , 101):
+    
     if x % 3 == 0 and x % 5 != 0:
         print("fizz " , end=" ")
+   
     elif x % 5 == 0 and x % 3 != 0:
         print("buzz ", end=" ")
+    
     elif x % 5 == 0 and x % 3 == 0:
         print("fizzbuzz ", end=" ")
+    
     else:
         print(x , end=" ")
 
